80da70db692e3f65c155fbe652260cb588c50ded
Samuel <Samuel@192.168.0.21>
2024-01-03 18:08:04 -0800

    initial commit

---
+++ src/d/DemoD.java
@@ -0,0 +1,11 @@
+package d;
+
+public class DemoD {
+	public static void main(String[] args) {
+		System.out.println("Thursday");
+		
+		System.out.println("github is here ");
+		System.out.println("java almost done ");
+	}
+
+}
